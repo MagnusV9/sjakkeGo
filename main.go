378311b73b474aaee9ef44dda49c56e4214295c8
Magnus <magnusvestvik@proton.me>
2023-07-08 14:25:41 +0200

    added lists with path to svg of pieces

--- main.go
+++ main.go
@@ -8,6 +8,43 @@ import (
 	"image/color"
 )
 
+var pathToPlayerBlackPieces = string[]{
+	"./assets/blackpieces/player/king.svg",
+	"./assets/blackpieces/player/queen.svg",
+	"./assets/blackpieces/player/rook.svg",
+	"./assets/blackpieces/player/bishop.svg",
+	"./assets/blackpieces/player/pawn.svg",
+	"./assets/blackpieces/player/knight.svg",
+}
+
+var pathToOpponentBlackPieces = string[]{
+	"./assets/blackpieces/opponent/king.svg",
+	"./assets/blackpieces/opponent/queen.svg",
+	"./assets/blackpieces/opponent/rook.svg",
+	"./assets/blackpieces/opponent/bishop.svg",
+	"./assets/blackpieces/opponent/pawn.svg",
+	"./assets/blackpieces/opponent/knight.svg",
+}
+
+var pathToPlayerWhitePieces = string[]{
+	"./assets/whitepieces/player/king.svg",
+	"./assets/whitepieces/player/queen.svg",
+	"./assets/whitepieces/player/rook.svg",
+	"./assets/whitepieces/player/bishop.svg",
+	"./assets/whitepieces/player/pawn.svg",
+	"./assets/whitepieces/player/knight.svg",
+}
+
+
+var pathToOpponentWhitePieces = string[]{
+	"./assets/whitepieces/opponent/king.svg",
+	"./assets/whitepieces/opponent/queen.svg",
+	"./assets/whitepieces/opponent/rook.svg",
+	"./assets/whitepieces/opponent/bishop.svg",
+	"./assets/whitepieces/opponent/pawn.svg",
+	"./assets/whitepieces/opponent/knight.svg",
+}
+
 type King struct {
 	Image  string
 	Player string
